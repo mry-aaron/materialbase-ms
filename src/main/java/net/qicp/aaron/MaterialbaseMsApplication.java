0779643e5f447f905b9b774c849743b14c4f734e
aaron_glb <aaron_glb@aliyun.com>
2019-03-07 19:11:04 +0800

    Initial commit

---
+++ src/main/java/net/qicp/aaron/MaterialbaseMsApplication.java
@@ -0,0 +1,13 @@
+package net.qicp.aaron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaterialbaseMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaterialbaseMsApplication.class, args);
+    }
+
+}
